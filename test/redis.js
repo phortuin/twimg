75c1902ce71c0350aa1cccc7e9ebc5a5131fa105
phortuin <phortuin@users.noreply.github.com>
2021-04-14 14:39:17 +0200

    feat: create redis-instance module to be able to read & write session data to Redis
    Signed-off-by: phortuin <phortuin@users.noreply.github.com>

---
+++ test/redis.js
@@ -0,0 +1,129 @@
+const test = require('tape')
+const proxyquire = require('proxyquire')
+const sinon = require('sinon')
+
+const AUTO_INCREMENT = 1
+
+/**
+ * Mock Redis connection options
+ */
+const REDIS_CONNECTION_OPTIONS = {
+	host: '127.0.0.1',
+	port: 1337,
+	password: 'hunter2',
+}
+
+/**
+ * Fake user credentials. Original object contains auth token etc, but we’re
+ * not testing against the object’s integrity, just that it’s there.
+ */
+const USER_CREDENTIALS = {
+	username: 'AzureDiamond',
+}
+
+/**
+ * The methods `incr`, `get` and `set` need to be promisifyable, so the last
+ * argument is a callback function that is called with `null` as first
+ * parameter (the `error` parameter; following "error-first callback style":
+ * see also https://nodejs.org/api/util.html#util_util_promisify_original
+ */
+const clientReturnObject = {
+	incr: (key, callback) => callback(null, AUTO_INCREMENT),
+	set: (key, val, callback) => callback(null, val),
+	on: (eventType, callback) => callback,
+}
+
+/**
+ * Mock redis package
+ */
+const redis = {
+	createClient: options => {
+		return {
+			...clientReturnObject,
+			get: (key, callback) => callback(null, JSON.stringify(USER_CREDENTIALS)),
+		}
+	}
+}
+
+/**
+ * As above, but the `get` method will return invalid JSON
+ */
+const redisNoUserCredentials = {
+	createClient: options => {
+		return {
+			...clientReturnObject,
+			get: (key, callback) => callback(null, ""),
+		}
+	}
+}
+
+/**
+ * Tests whether redis.createClient is called, which indicates the redis client
+ * is being initialised.
+ *
+ * Note: spy.args[0][0] is first call to the spied function, first argument
+ * See also https://sinonjs.org/releases/v10.0.1/spies/ (under spy.args)
+ *
+ * Note: error handling isn’t tested since the setup would require implementing
+ * an EventEmitter and testing whether the event is handled and thrown, which
+ * is basically testing if Node.js’s internals work.
+ */
+test('Initialising client passes correct connection params to Redis client', t => {
+	const redisInstance = proxyquire('../lib/redis-instance', {
+		redis
+	})
+	const spy = sinon.spy(redis, 'createClient')
+	redisInstance.initClient(REDIS_CONNECTION_OPTIONS)
+	t.deepLooseEqual(spy.args[0][0], { ...REDIS_CONNECTION_OPTIONS, tls: {} })
+	t.end()
+})
+
+/**
+ * Tests whether the session id is returned when creating a session. The session
+ * is valid if it’s a URL safe base64 encoded string of 32 characters plus the
+ * AUTO_INCR added at the end.
+ *
+ * The session id length of 32 breaks down like this:
+ * It’s made up of 24 bytes, base64 encoded
+ * ...meaning, 4 bytes for every 3
+ * ...24×4÷3 = 24×1⅓ = 32
+ *
+ * Note: we’re not testing if user credentials were actually stored in Redis.
+ */
+test('Create user session returns a valid session id', async t => {
+	const redisInstance = proxyquire('../lib/redis-instance', {
+		redis
+	})
+	const sessionId = await redisInstance.createUserSession()
+	const sessionRegex = new RegExp(`^[a-zA-Z0-9_-]{32}${AUTO_INCREMENT}$`)
+	t.ok(sessionRegex.test(sessionId))
+	t.end()
+})
+
+
+/**
+ * Tests whether user credentials are returned properly. This test is pretty
+ * lousy, since all it effectively does is JSON.parse something that we JSON
+ * stringified above here in the mocked `get` method
+ */
+test('Get user session should return user credentials', async t => {
+	const client = proxyquire('../lib/redis-instance', {
+		redis
+	})
+	const userCredentials = await client.getUserSession()
+	t.deepLooseEqual(USER_CREDENTIALS, userCredentials)
+	t.end()
+})
+
+/**
+ * Tests whether getUserSession throws if there are no (parseable) user
+ * credentials. Again, pretty lousy, since all we do is test an empty string
+ * against a try-catch block with JSON.parse
+ */
+test('Get user session should throw if it returns bad user credentials', async t => {
+	const client = proxyquire('../lib/redis-instance', {
+		redis: redisNoUserCredentials
+	})
+	t.throws(await client.getUserSession())
+	t.end()
+})
