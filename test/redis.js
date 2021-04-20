356bb057a4e12a50892631c257683c6fdd870e5c
phortuin <phortuin@users.noreply.github.com>
2021-04-20 21:56:04 +0200

    feat: add delete user session method to redis instance, improve tests
    Signed-off-by: phortuin <phortuin@users.noreply.github.com>

--- test/redis.js
+++ test/redis.js
@@ -22,6 +22,16 @@ const USER_CREDENTIALS = {
 }
 
 /**
+ * Fake valid session id
+ */
+const SESSION_ID = `ABCDEFGHIJKLMnopqrstuvwxyz012345${AUTO_INCREMENT}`
+
+/**
+ * Fake invalid session id (could be a valid Redis key though)
+ */
+ const INVALID_SESSION_ID = 'auto_increment'
+
+/**
  * The methods `incr`, `get` and `set` need to be promisifyable, so the last
  * argument is a callback function that is called with `null` as first
  * parameter (the `error` parameter; following "error-first callback style":
@@ -31,6 +41,7 @@ const clientReturnObject = {
 	incr: (key, callback) => callback(null, AUTO_INCREMENT),
 	set: (key, val, callback) => callback(null, val),
 	on: (eventType, callback) => callback,
+	del: (key, callback) => callback(null, key),
 }
 
 /**
@@ -57,6 +68,20 @@ const redisNoUserCredentials = {
 	}
 }
 
+// Spies
+const createClientSpy = sinon.spy(redis, 'createClient')
+const redisDelSpy = sinon.spy(clientReturnObject, 'del')
+
+// Init redisInstance
+const redisInstance = proxyquire('../lib/redis-instance', {
+	redis
+})
+
+// Init redisInstance with broken user credentials
+const brokenRedisInstance = proxyquire('../lib/redis-instance', {
+	redis: redisNoUserCredentials
+})
+
 /**
  * Tests whether redis.createClient is called, which indicates the redis client
  * is being initialised.
@@ -69,37 +94,23 @@ const redisNoUserCredentials = {
  * is basically testing if Node.js’s internals work.
  */
 test('Initialising client passes correct connection params to Redis client', t => {
-	const redisInstance = proxyquire('../lib/redis-instance', {
-		redis
-	})
-	const spy = sinon.spy(redis, 'createClient')
 	redisInstance.initClient(REDIS_CONNECTION_OPTIONS)
-	t.deepLooseEqual(spy.args[0][0], { ...REDIS_CONNECTION_OPTIONS, tls: {} })
+	t.deepLooseEqual(createClientSpy.args[0][0], { ...REDIS_CONNECTION_OPTIONS, tls: {} })
 	t.end()
 })
 
 /**
- * Tests whether the session id is returned when creating a session. The session
- * is valid if it’s a URL safe base64 encoded string of 32 characters plus the
- * AUTO_INCR added at the end.
- *
- * The session id length of 32 breaks down like this:
- * It’s made up of 24 bytes, base64 encoded
- * ...meaning, 4 bytes for every 3
- * ...24×4÷3 = 24×1⅓ = 32
+ * Tests whether a valid session id is returned when creating a session. The
+ * regex is the same as the one in `isValidSessionId` in redis-instance.js
  *
  * Note: we’re not testing if user credentials were actually stored in Redis.
  */
 test('Create user session returns a valid session id', async t => {
-	const redisInstance = proxyquire('../lib/redis-instance', {
-		redis
-	})
 	const sessionId = await redisInstance.createUserSession()
 	const sessionRegex = new RegExp(`^[a-zA-Z0-9_-]{32}${AUTO_INCREMENT}$`)
 	t.ok(sessionRegex.test(sessionId))
 	t.end()
 })
-
 
 /**
  * Tests whether user credentials are returned properly. This test is pretty
@@ -107,11 +118,29 @@ test('Create user session returns a valid session id', async t => {
  * stringified above here in the mocked `get` method
  */
 test('Get user session should return user credentials', async t => {
-	const client = proxyquire('../lib/redis-instance', {
-		redis
-	})
-	const userCredentials = await client.getUserSession()
+	const userCredentials = await redisInstance.getUserSession(SESSION_ID)
 	t.deepLooseEqual(USER_CREDENTIALS, userCredentials)
+	t.end()
+})
+
+/**
+ * Tests whether deleteUserSession calls the DEL command from the redis client.
+ */
+test('Delete user session should try to DEL session id from Redis', async t => {
+	await redisInstance.deleteUserSession(SESSION_ID)
+	t.ok(redisDelSpy.calledOnce)
+	t.end()
+})
+
+/**
+ * Get/delete user session functions should only return/delete valid session ids
+ * or else any Redis key can be accessed/deleted
+ */
+test('Delete or get invalid session ID should fail', async t => {
+	const userCredentials = await redisInstance.getUserSession(INVALID_SESSION_ID)
+	await redisInstance.deleteUserSession(INVALID_SESSION_ID)
+	t.notDeepLooseEqual(USER_CREDENTIALS, userCredentials)
+	t.notOk(redisDelSpy.calledTwice) // First time is in test above
 	t.end()
 })
 
@@ -120,10 +149,7 @@ test('Get user session should return user credentials', async t => {
  * credentials. Again, pretty lousy, since all we do is test an empty string
  * against a try-catch block with JSON.parse
  */
-test('Get user session should throw if it returns bad user credentials', async t => {
-	const client = proxyquire('../lib/redis-instance', {
-		redis: redisNoUserCredentials
-	})
-	t.throws(await client.getUserSession())
+test('Get user session should return {} if Redis has bad user credentials', async t => {
+	t.deepLooseEqual(await brokenRedisInstance.getUserSession(SESSION_ID), {})
 	t.end()
 })
