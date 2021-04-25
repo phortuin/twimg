6859be0aaebf2981e8e1ab6a3c6558500e5d46c7
phortuin <phortuin@users.noreply.github.com>
2021-04-25 14:25:32 +0200

    chore: write jsdocs
    Signed-off-by: phortuin <phortuin@users.noreply.github.com>

--- api/auth.js
+++ api/auth.js
@@ -1,6 +1,10 @@
 const twitter = require('../lib/twitter-instance')
 const { handleErrors } = require('../lib/middleware')
 
+/**
+ * Kicks off the oAuth process. Requests an oauth token from Twitter, and
+ * redirects the user to the Twitter login screen.
+ */
 module.exports = handleErrors(async (req, res) => {
 	const { oauth_token } = await twitter.getRequestToken(req)
 	res.writeHead(302, {
--- api/list.js
+++ api/list.js
@@ -3,8 +3,9 @@ const { getTimeline } = require('../lib/twitter-instance')
 const render = require('../lib/render')
 
 /**
- * Gets timeline data from twitter and renders them. This route needs
- * an authenticated twitter client, which is handled by the authenticate
+ * Gets home timeline from twitter and renders it. If an `id` query parameter
+ * is available, this id will be used as an offset id (pagination). This route
+ * needs an authenticated twitter client, which is handled by the authenticate
  * middleware.
  */
 module.exports = authenticate(handleErrors(async (req, res) => {
